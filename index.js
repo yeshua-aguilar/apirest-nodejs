5ea841f51fb7fda37e6f8e1bcbcef4c90492e051
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2022-07-01 09:59:26 -0500

    api 4

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ app.use('/api/users', Users);
 app.use('/api/rest', Rests);
 
 
+const port = process.env.PORT || 4000;
+
+
 mongoose.connect('mongodb://yeshua:yeshua2@ac-a8gqmio-shard-00-00.ypwvpkm.mongodb.net:27017,ac-a8gqmio-shard-00-01.ypwvpkm.mongodb.net:27017,ac-a8gqmio-shard-00-02.ypwvpkm.mongodb.net:27017/?ssl=true&replicaSet=atlas-xrugto-shard-0&authSource=admin&retryWrites=true&w=majority', 
     { useNewUrlParser: true }, (err, res) => {
     err && console.log("Error conectando a la base de datos");
-    app.listen(4000, () => {
+    app.listen(port, () => {
         console.log("Servidor corriendo en http://localhost:4000");
     });
 });
